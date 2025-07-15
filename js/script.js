480516240d57be300d065c643bbcf0154d4b6246
akmal-azhar <akmalazharwork@gmail.com>
2025-07-15 10:46:40 +0800

    Commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function openModal(title, videoURL, description) {
+  document.getElementById('videoModalLabel').textContent = title;
+  document.getElementById('videoFrame').src = videoURL;
+  document.getElementById('videoDesc').textContent = description;
+
+  const myModal = new bootstrap.Modal(document.getElementById('videoModal'));
+  myModal.show();
+}
+
+function stopVideo() {
+  document.getElementById('videoFrame').src = '';
+}
+function filterVlogs() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const cards = document.querySelectorAll(".vlog-card");
+
+  cards.forEach((card) => {
+    const title = card.querySelector(".card-title").textContent.toLowerCase();
+    if (title.includes(input)) {
+      card.parentElement.style.display = "block"; // .col
+    } else {
+      card.parentElement.style.display = "none";
+    }
+  });
+}
+// Example: alert bila user submit contact form
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('form');
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      alert('Thank you for your message!');
+      form.reset();
+    });
+  }
+});
